8b03aa20d2e8621be421499cdf58cac4287d936f
anushamahajan5 <anusha.mahajan.ece21@itbhu.ac.in>
2024-10-22 00:08:42 +0530

    Connected to database

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+ 
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+import { Token } from "graphql";
+
+import { connectDB } from "./db/connectDB.js";
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+ 
+app.use(
+	'/',
+	cors(),
+	express.json(),
+	// expressMiddleware accepts the same arguments:
+	// an Apollo Server instance and optional configuration options
+	expressMiddleware(server, {
+		context: async ({ req }) => ({ token: req.headers.token }),
+	}),
+);
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
